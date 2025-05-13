98ced76322117e2f74c61383ffddb9345a9c0531
artemgon <artemhoncharuk123@gmail.com>
2025-05-13 19:07:44 +0300

    Task 1 is created.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace project_15_04_25
+﻿using System.Threading;
+
+namespace project_15_04_25
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private readonly Thread _thread;
+
+        public Program()
         {
-            Console.WriteLine("Hello, World!");
+            _thread = new (WorkerMethod);
+            _thread.Start();
+        }
+
+        void WorkerMethod()
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Main()
+        {
+            _ = new Program();
         }
     }
 }
