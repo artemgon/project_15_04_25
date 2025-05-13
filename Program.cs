9b4b0627f6f7998a1745e073a2d5e5b0e3fe3a12
artemgon <artemhoncharuk123@gmail.com>
2025-05-13 18:43:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace project_15_04_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
