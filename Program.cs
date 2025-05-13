0bdbb44b7e53d693270e152afc0e271a31d536a5
artemgon <artemhoncharuk123@gmail.com>
2025-05-13 19:12:18 +0300

    Task 2 is created.

--- Program.cs
+++ Program.cs
@@ -8,13 +8,18 @@ namespace project_15_04_25
 
         public Program()
         {
-            _thread = new (WorkerMethod);
+            int start, end;
+            Console.WriteLine("Enter start number: ");
+            start = int.Parse(Console.ReadLine() ?? "0");
+            Console.WriteLine("Enter end number: ");
+            end = int.Parse(Console.ReadLine() ?? "0");
+            _thread = new (() => WorkerMethod(start, end));
             _thread.Start();
         }
 
-        void WorkerMethod()
+        void WorkerMethod(int start, int end)
         {
-            for (int i = 0; i <= 50; i++)
+            for (int i = start; i <= end; i++)
             {
                 Console.WriteLine(i);
             }
